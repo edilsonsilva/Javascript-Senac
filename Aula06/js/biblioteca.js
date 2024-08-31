f0810a00a464dabfbad84c6dd5f0f39b6fcd5ca8
Edilson Silva <edilson@yahoo.com.br>
2024-08-31 12:02:19 -0300

    Funcoes

---
+++ Aula06/js/biblioteca.js
@@ -0,0 +1,15 @@
+function somaArray(array) {
+    let rs = 0
+    for (let i = 0; i <= array.length - 1; i++) {
+        rs += array[i]
+    }
+    return rs
+}
+
+function mediaArray(array) {
+    let rs = 0
+    for (let i = 0; i <= array.length - 1; i++) {
+        rs += array[i]
+    }
+    return rs / array.length
+}
---
+++ Aula06/js/uso.js
@@ -0,0 +1,7 @@
+let num = [10, 5, 60, 3, 24, 12, 30]
+
+alert(somaArray(num))
+document.getElementById("resultado").value = somaArray(num)
+
+alert(mediaArray(num))
+document.getElementById("resultado2").value = mediaArray(num)
